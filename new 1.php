820389302447b3a72f9635d8ff30038db2894b3c
chosen1onestar <74967017+chosen1onestar@users.noreply.github.com>
2022-09-14 15:41:05 +0200

    Add files via upload

---
+++ new 1.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <h1>Hello World!</h1>
+</head>
+<body>
+<?php
+	$mycounter = 1;
+	echo "Hello world"."<br>". $mycounter."<br>". "Hello friend"; 
+	
+	$x = 0;
+	if ($x == 0){
+		echo "Nie masz wiadomosci";
+	}
+	else{
+		echo "masz". $x ."wiadomosci";
+	}
+	
+?>
+</body>
+</html>
